3c135830985f016f791755b0428c83368b078afd
juantoranzos <toranzos628@gmail.com>
2023-04-18 19:44:06 -0300

    agregue la logica de la pagina

---
+++ js/app.js
@@ -0,0 +1,24 @@
+let temaConfigurado = JSON.parse(localStorage.getItem('tema')) || 'dark';
+console.log(temaConfigurado);
+cambiarTema(temaConfigurado);
+
+document
+  .getElementById("btnThemeDark")
+  .addEventListener("click", () => cambiarTema("dark"));
+document
+  .getElementById("btnThemeLight")
+  .addEventListener("click", () => cambiarTema("light"));
+
+function cambiarTema(color) {
+  let body = document.querySelector("body");
+  //let body = document.getElementsByTagName('body');
+  body.setAttribute("data-bs-theme", color);
+  //actualizar el icono
+  color === "dark"
+    ? (document.getElementById("iconTheme").className = "bi bi-moon-stars-fill")
+    : (document.getElementById("iconTheme").className =
+        "bi bi-brightness-high-fill");
+
+    //guardar color en local storage
+    localStorage.setItem('tema', JSON.stringify(color))
+}
